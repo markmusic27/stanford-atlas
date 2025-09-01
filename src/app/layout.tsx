071faca094fd229d9a4870850dd69582de36acaa
Mark Music <me@markmusic.io>
2025-09-01 14:58:30 -0600

    chr: add custom ui colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${geist.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
+      <body className="bg-primary-2">{children}</body>
     </html>
   );
 }
