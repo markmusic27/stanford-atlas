f185c65392faaa53bc32d207f5a4ad2e2d693873
Mark Music <me@markmusic.io>
2025-09-02 17:24:39 -0600

    feat: finish styling the peripherals and add redirrect logic

--- src/components/peripherals/Peripherals.tsx
+++ src/components/peripherals/Peripherals.tsx
@@ -1,12 +1,38 @@
 import PeripheralCard from "./components/PeripheralCard";
+import { redirect } from "next/navigation";
 
 const Peripherals = () => {
   return (
     <div className="flex flex-row justify-center gap-[6px]">
-      <PeripheralCard title={"Sign In"} />
-      <PeripheralCard title={"Why use this over Navigator"} />
-      <PeripheralCard title={"Build a 4-year plan"} />
-      <PeripheralCard title={"GitHub"} />
+      <PeripheralCard
+        title={"Sign In"}
+        onAction={async () => {
+          "use server";
+          console.log("hello world");
+        }}
+      />
+      <PeripheralCard
+        title={"Why use this over Navigator"}
+        onAction={async () => {
+          "use server";
+          console.log("hello world");
+        }}
+      />
+      <PeripheralCard
+        title={"Build a 4-year plan"}
+        onAction={async () => {
+          "use server";
+          console.log("hello world");
+        }}
+      />
+      <PeripheralCard
+        title={"GitHub"}
+        onAction={async () => {
+          "use server";
+          console.log("hello world");
+          redirect("https://github.com/markmusic27/stanford-atlas");
+        }}
+      />
     </div>
   );
 };
--- src/components/peripherals/components/PeripheralCard.tsx
+++ src/components/peripherals/components/PeripheralCard.tsx
@@ -1,14 +1,22 @@
+"use client";
+
 import CursorScale from "~/components/ui/CursorScale";
 import Icon, { IconType } from "~/components/ui/icons/Icon";
 
 interface PeripheralCardProps {
   title: string;
+  onAction?: (title: string) => Promise<void>;
 }
 
-const PeripheralCard = ({ title }: PeripheralCardProps) => {
+const PeripheralCard = ({ title, onAction }: PeripheralCardProps) => {
   return (
     <CursorScale hoverScale={1.01} maxTranslate={1}>
-      <div className="bg-primary-6/[0.7] flex h-[30px] cursor-pointer flex-row items-center gap-[4px] rounded-[10px] pr-[8px] pl-[10px]">
+      <div
+        className="bg-primary-6/[0.7] flex h-[30px] cursor-pointer flex-row items-center gap-[4px] rounded-[10px] pr-[8px] pl-[10px]"
+        onClick={() => {
+          if (onAction) void onAction(title);
+        }}
+      >
         <Icon
           type={IconType.Slash}
           width={6}
