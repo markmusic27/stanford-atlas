8dadf6fe288281beec5e298c8358fe1b6706e2c6
Mark Music <me@markmusic.io>
2025-09-02 18:05:41 -0600

    feat: add general style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,11 +12,14 @@ export default function HomePage() {
   return (
     <main className="relative h-screen w-full">
       {/* Chat Window */}
-      <div className="mx-auto flex h-full w-full max-w-[800px] flex-col px-[16px]">
-        <div className="h-[200px]" /> {/* TEMPORARY */}
+      <div className="mx-auto flex h-full w-full max-w-[800px] flex-col px-[8px] md:px-[16px]">
+        <div className="flex-1" />
+        <img src="/brand/logo.svg" alt="logo" className="h-[40px]" />
+        <div className="h-[10dvh] max-h-[95px] min-h-[40px]" />
         <SearchBar action={submitAction} />
         <div className="h-[32px]" />
         <Peripherals />
+        <div className="flex-2" />
       </div>
     </main>
   );
