ada0977ed718a5eb2d57ca9e7ef9f6282095dccc
Mark Music <me@markmusic.io>
2025-09-08 19:03:26 -0600

    feat: finish message box

--- src/components/ui/chat/Message.chat.tsx
+++ src/components/ui/chat/Message.chat.tsx
@@ -36,26 +36,27 @@ const Message = ({ message }: MessageProps) => {
     <div className="flex w-full justify-end">
       <div
         ref={containerRef}
-        className={`bg-primary-5 relative ${isExpanded ? "max-h-none" : "max-h-[37dvh]"} max-w-[75%] overflow-clip rounded-[16px] px-[24px] py-[16px]`}
+        className={`bg-primary-5 relative ${isExpanded ? "max-h-[2000px]" : "max-h-[37dvh]"} max-w-[75%] overflow-clip rounded-[16px] px-[24px] py-[16px] transition-[max-height] duration-600 ease-in-out`}
       >
         <p className="text-primary-text z-0 text-[16px] leading-relaxed whitespace-pre-wrap">
           {message.payload.content}
         </p>
-        {isOverflowing && !isExpanded ? (
+        {isOverflowing || isExpanded ? (
           <div
             className="pointer-events-none absolute inset-x-0 bottom-0 z-1 h-[60px]"
             style={{
-              background:
-                "linear-gradient(to top, var(--color-primary-5) 0%, var(--color-primary-5) 20%, transparent 100%)",
+              background: isExpanded
+                ? "transparent"
+                : "linear-gradient(to top, var(--color-primary-5) 0%, var(--color-primary-5) 20%, transparent 100%)",
             }}
           >
             <div
-              className="bg-primary-7 pointer-events-auto absolute right-[12px] bottom-[12px] h-[25px] w-[25px] cursor-pointer rounded-full hover:scale-[1.1]"
+              className="bg-primary-7 pointer-events-auto absolute right-[12px] bottom-[12px] grid h-[25px] w-[25px] cursor-pointer place-items-center rounded-full transition-all duration-200 hover:scale-[1.05]"
               onClick={() => setIsExpanded((prev) => !prev)}
             >
               <Icon
                 type={IconType.Chevron}
-                className={`text-secondary-text-1 pb-[3px] transition-transform duration-300 ${isExpanded ? "rotate-180" : ""}`}
+                className={`text-secondary-text-1 pb-[3px] transition-[rotate] duration-200 ${isExpanded ? "rotate-180" : ""}`}
               />
             </div>
           </div>
