36afaaf3c3ceab8de6ca9710f426b2d92d44b82b
Mark Music <me@markmusic.io>
2025-10-05 21:04:00 -0700

    feat: revert back to origin stuff

--- src/utils/auth/signIn.ts
+++ src/utils/auth/signIn.ts
@@ -4,11 +4,12 @@ import { createClient } from "~/utils/supabase/client";
 
 export async function signInWithGoogle(nextPath = "/") {
   const supabase = createClient();
+  const origin = window.location.origin;
 
   const { error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: `https://stanfordatlas.com${nextPath}`,
+      redirectTo: `${origin}${nextPath}`,
     },
   });
 
