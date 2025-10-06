ecca5cea9330580925de70f90845d78e66e93726
Mark Music <me@markmusic.io>
2025-10-05 20:49:40 -0700

    fix: try to fix auth redirect

--- src/utils/auth/signIn.ts
+++ src/utils/auth/signIn.ts
@@ -4,14 +4,11 @@ import { createClient } from "~/utils/supabase/client";
 
 export async function signInWithGoogle(nextPath = "/") {
   const supabase = createClient();
-  const envSiteUrl = process.env.NEXT_PUBLIC_SITE_URL;
-  const origin =
-    envSiteUrl && envSiteUrl.length > 0 ? envSiteUrl : window.location.origin;
 
   const { error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: `${origin}${nextPath}`,
+      redirectTo: `https://stanfordatlas.com${nextPath}`,
     },
   });
 
