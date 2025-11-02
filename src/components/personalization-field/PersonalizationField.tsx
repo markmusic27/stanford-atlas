5d6367306ecc327ee398c55f20f4f8a6fa05e18c
Mark Music <me@markmusic.io>
2025-11-01 21:16:53 -0700

    feat: change animation in preferences

--- src/components/personalization-field/PersonalizationField.tsx
+++ src/components/personalization-field/PersonalizationField.tsx
@@ -19,26 +19,26 @@ const PersonalizationField = ({
 }: PersonalizationFieldProps) => {
   return (
     <>
-      <p className="text-primary-text text-[18px]">{title}</p>
-      <div className="h-[12px]" />
       <AnimatePresence mode="wait">
         {loading ? (
           <motion.div
             key="skeleton"
             initial={{ opacity: 1 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -6 }}
             transition={{ duration: 0.3 }}
-            className="bg-primary-1 border-primary-9 h-[80px] w-full animate-pulse rounded-[12px] border-[1px]"
+            className="bg-primary-4 border-primary-9 h-[80px] w-full animate-pulse rounded-[12px] border-[1px]"
           />
         ) : (
           <motion.div
             key="textarea"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
+            initial={{ opacity: 0, y: 6 }}
+            animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0 }}
             transition={{ duration: 0.3 }}
           >
+            <p className="text-primary-text text-[18px]">{title}</p>
+            <div className="h-[12px]" />
             <CustomTextArea
               placeholder={placeholder}
               defaultValue={defaultValue}
